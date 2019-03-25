6535d97086aef8c3a710746afe60443e67a279f5
sterlu <sterlu.sd@gmail.com>
2019-03-25 17:33:10 +0100

    Remove "updated at" from playlist tile

--- src/components/Explore/PlaylistTile/PlaylistTile.js
+++ src/components/Explore/PlaylistTile/PlaylistTile.js
@@ -8,12 +8,7 @@ class PlaylistTile extends PureComponent {
       <div className="playlist-tile-wrapper">
         <div className="cover" style={{ backgroundImage: `url(${playlist.cover})` }}>
           <div className="details">
-            <p><b>by {playlist.owner.name}</b></p>
-            <p><b>{playlist.tracks.spotify.total} tracks</b></p>
-            {
-              playlist.updated &&
-              <p><b>updated {new Date(parseInt(playlist.updated)).toISOString().substr(0, 10)}</b></p>
-            }
+            <p><b>by {playlist.owner.name} | {playlist.tracks.spotify.total} tracks</b></p>
             <p className="description" dangerouslySetInnerHTML={{ __html: playlist.description }} />
             <div className="actions">
               <a onClick={() => this.props.play(playlist)} title="Play">▶️ Play</a>
